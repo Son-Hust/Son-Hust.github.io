4e78d0a16d938423e3f814ca17ba457dc8c8c8c7
Your Name <you@example.com>
2021-09-28 13:25:11 +0700

    111

--- js/myJs.js
+++ js/myJs.js
@@ -127,3 +127,22 @@ $('#yes').click(function () {
     })
 })
 
+window.addEventListener('load', function () {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+    var source = audioCtx.createBufferSource();
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'soundd/background.mp3');
+    xhr.responseType = 'arraybuffer';
+    xhr.addEventListener('load', function (r) {
+        audioCtx.decodeAudioData(
+                xhr.response, 
+                function (buffer) {
+                    source.buffer = buffer;
+                    source.connect(audioCtx.destination);
+                    source.loop = false;
+                });
+        source.start(0);
+    });
+    xhr.send();
+});
+
